8aa989fba10a72c73d316559a7ad578174a23bb6
AYJF <jimenezfernandezalvaro@gmail.com>
2022-11-01 18:20:36 -0400

    Initial Commit

---
+++ MB-Control/MB_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MB_ControlApp.swift
+//  MB-Control
+//
+//  Created by Alvaro Jimenez on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MB_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
